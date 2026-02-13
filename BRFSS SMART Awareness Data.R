8422ea6419ea586c51edfb296219f9632a5a0c4b
dhritibharadwaj <dhriti.t.bharadwaj@gmail.com>
2026-02-13 15:28:32 -0800

    Removed Year Parameters for Awareness Data--Useful to know how demographic changes impacts awareness

--- BRFSS SMART Awareness Data.R
+++ BRFSS SMART Awareness Data.R
@@ -2,7 +2,6 @@ library(readr)
 data <- read_csv("C:/Users/dhrit/Downloads/Behavioral_Risk_Factors__Selected_Metropolitan_Area_Risk_Trends_(SMART)_MMSA_Prevalence_Data_(2011_to_Present)_20260213.csv")
 library(dplyr)
 df_brfss <- data %>%
-  filter(Class == "Hypertension Awareness",
-         Year == "2023")
+  filter(Class == "Hypertension Awareness")
 
 
