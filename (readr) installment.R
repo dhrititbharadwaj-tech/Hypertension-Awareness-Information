61b3049445b0e2508d046895b68190eff3860171
dhritibharadwaj <dhriti.t.bharadwaj@gmail.com>
2026-02-13 15:21:01 -0800

    New Data Update

---
+++ (readr) installment.R
@@ -0,0 +1,10 @@
+install.packages("readr")   # only once
+library(readr)
+
+raw <- read_fwf("data/MMSA23.ASC")
+
+head(raw)
+
+
+
+
---
+++ hello world.R
@@ -0,0 +1,36 @@
+"Hello World"
+name <- "John"
+age <- 40
+name
+age
+text <- "is Awesome"
+paste("R", text)
+r <- "R"
+paste (r, text)
+
+var1 <- var2 <- var3 <- "orange"
+print (var1)
+print (var2)
+print (var3)
+
+#Types of data are numeric, integer, complex, character, logical
+#class command shows what type of data the variable includes
+# numeric
+x <- 10.5
+class(x)
+
+# integer
+x <- 1000L
+class(x)
+
+# complex
+x <- 9i + 3
+class(x)
+
+# character/string
+x <- "R is exciting"
+class(x)
+
+# logical
+x <- TRUE
+class(x)
