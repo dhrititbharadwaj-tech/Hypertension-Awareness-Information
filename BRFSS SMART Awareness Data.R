dde9db414eb8be435f606b2d8d2a48b7ff88f90d
dhritibharadwaj <dhriti.t.bharadwaj@gmail.com>
2026-02-16 13:37:21 -0800

    Cut CountyAwareness to only required columns

--- BRFSS SMART Awareness Data.R
+++ BRFSS SMART Awareness Data.R
@@ -1,7 +1,13 @@
 library(readr)
-data <- read_csv("C:/Users/dhrit/Downloads/Behavioral_Risk_Factors__Selected_Metropolitan_Area_Risk_Trends_(SMART)_MMSA_Prevalence_Data_(2011_to_Present)_20260213.csv")
+brfss <- read_csv("C:/Users/dhrit/Downloads/Behavioral_Risk_Factors__Selected_Metropolitan_Area_Risk_Trends_(SMART)_MMSA_Prevalence_Data_(2011_to_Present)_20260213.csv")
 library(dplyr)
-df_brfss <- data %>%
-  filter(Class == "Hypertension Awareness")
+CountyAwareness <- brfss %>%
+  filter(Class == "Hypertension Awareness",
+         Response == "Yes")
+
+#cut down to only columns you want (year, location, and hypertension awareness data value)
+
+Only_CountyAwareness <- CountyAwareness[, c("Year", "Locationdesc", "Data_value")]
+
 
 
