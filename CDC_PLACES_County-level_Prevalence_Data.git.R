4d85ef8747e666eb64f27b82a19c51b4967970d9
dhritibharadwaj <dhriti.t.bharadwaj@gmail.com>
2026-02-23 00:27:33 -0800

    random commit

--- CDC_PLACES_County-level_Prevalence_Data.git.R
+++ CDC_PLACES_County-level_Prevalence_Data.git.R
@@ -1,12 +1,12 @@
 library(readr)
 #First, we want to upload the CDC PLACES data onto this repository:
 data <- read_csv("C:/Users/dhrit/Downloads/PLACES__Local_Data_for_Better_Health,_County_Data,_2025_release_20260108.csv")
-#Now we want to filter this dataset for only hypertension-related information, or more specifically, only cells
-#under the column "Measure" with the content 'High blood pressure among adults"; specifically, we are looking for age-adjusted prevalence
+#Now we want to filter this data set for only hypertension-related information, or more specifically, only cells
+#under the column "Measure" with the content 'High blood pressure among adults"; specifically, we are looking for crude prevalence
 #only, so we also add in another filter where "Data_Value_Type" is filtered for this.
 library(dplyr)
-high_bp_data <- data %>%
+county_data <- data %>%
   filter(Measure == "High blood pressure among adults",
-         Data_Value_Type == "Age-adjusted prevalence")
+         Data_Value_Type == "Crude prevalence")
 
 
--- CDC_PLACES_ZTCA-level_Prevelance_Data.R
+++ CDC_PLACES_ZTCA-level_Prevelance_Data.R
@@ -1,7 +1,7 @@
 library(readr)
 data <- read_csv("C:/Users/dhrit/Downloads/PLACES__Local_Data_for_Better_Health,_ZCTA_Data,_2025_release_20260213.csv")
 library(dplyr)
-high_bp_data <- data %>%
+ztca_data <- data %>%
   filter(Measure == "High blood pressure among adults",
          Data_Value_Type == "Crude prevalence")
 
